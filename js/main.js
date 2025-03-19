0ba909769afbaf464d2de136698ae1a199eab11b
AntonyDias <antonygabrielamd@gmail.com>
2025-03-18 21:43:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let itens = container.querySelectorAll('.list .item')
+let indicador = document.querySelector('.indicadores')
+let dots = indicador.querySelectorAll('ul li')
+let list = container.querySelector('.list')
+
+let active = 0
+let firstPosition = 0
+let lastPosition = itens.length - 1
+
+function setSlider() {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    let dotsOld = indicador.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+
+    indicador.querySelector('.numeros').innerHTML = '0' + (active + 1)
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+    active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
+    itens[active].classList.add('active')    
+}
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    setSlider()
+    itens[active].classList.add('active')
+}
